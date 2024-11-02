c9f7625f57c12eeb1bbbad033a17660a1588aecb
rioto.nagano <kingdom950927@gmail.com>
2024-11-03 08:58:51 +0900

    feat:profilesテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_02_233911) do
+ActiveRecord::Schema.define(version: 2024_11_02_234407) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -18,4 +26,5 @@ ActiveRecord::Schema.define(version: 2024_11_02_233911) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "profiles", "users"
 end
