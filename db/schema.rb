f6e06d0c1f726e09a1c921fb12575d5e5c39ec5e
rioto.nagano <kingdom950927@gmail.com>
2024-11-03 09:01:38 +0900

    feat:groupsテーブルとGroupモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_02_234407) do
+ActiveRecord::Schema.define(version: 2024_11_03_000040) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string "name"
@@ -26,5 +34,6 @@ ActiveRecord::Schema.define(version: 2024_11_02_234407) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "groups", "users"
   add_foreign_key "profiles", "users"
 end
